f0a2156cf0b15fe3e5c76b9fbad87f8c917e63b3
Asus <chinjf-wm19@student.tarc.edu.my>
2020-07-02 19:41:09 +0800

    commit by chin joel fei

--- app/src/main/java/com/example/practical012partb/MainActivity.java
+++ app/src/main/java/com/example/practical012partb/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.practical012partb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,14 +23,23 @@ Toast toast = Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
 
 toast.show();
 }
-public void countUp(View view){
-++mCount;
-        if (mShowCount !=null)
-    mShowCount.setText(Integer.toString(mCount));
+public void countUp(View view) {
+    ++mCount;
+    if (mShowCount != null)
+        mShowCount.setText(Integer.toString(mCount));
+    int number = mCount % 2;
+    if (number ==0) {
+        view.setBackgroundColor(Color.GREEN);
+    }
+    else
+        view.setBackgroundColor(Color.MAGENTA);
+    findViewById(R.id.button_zero).setBackgroundColor(Color.MAGENTA);
 }
 
 public void resetCount(View view){
 mCount=0;
 mShowCount.setText(String.valueOf(mCount));
 }
+
+
 }
